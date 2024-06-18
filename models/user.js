30898d625f659bde24d8051c3cae379251ae33eb
TENSAEA <tensaeaschalew26@gmail.com>
2024-06-18 10:19:53 +0300

    update

--- models/user.js
+++ models/user.js
@@ -50,9 +50,9 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  verificationStatus: {
+  status: {
     type: String,
-    default: "not active",
+    default: "unverified",
   },
   isBlocked: {
     type: String,
